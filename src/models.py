74aa5179d6d21e5616fc9c17ea5556b570c60f16
Carlos Alzate <158311146+cdalzate91@users.noreply.github.com>
2024-09-09 16:05:59 +0000

    Creación de las primeras Clases

--- src/models.py
+++ src/models.py
@@ -1,29 +1,83 @@
+import enum
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class User(Base):
+#     __tablename__ = 'user'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('user.id'))
+#     person = relationship(User)
+class MyEnum(enum.Enum):
+    videos = 1
+    imagenes = 2
+    historias = 3
+    post = 4
+
+    def to_dict(self):
+        return {}
+
+class User(Base):
+    __tablename__='User'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+    def to_dict(self):
+        return {}
+
+class Post(Base):
+    __tablename__='Post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'))
+
+    def to_dict(self):
+        return {}
+
+class Media(Base):
+    __tablename__='Media'
+    id = Column(Integer, primary_key=True)
+    media_type = Column(Enum(MyEnum), nullable=False)
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+    def to_dict(self):
+        return {}
+
+class Comment(Base):
+    __tablename__='Comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('User.id'))
+    post_id = Column(Integer, ForeignKey('Post.id'))
+
+    def to_dict(self):
+        return {}
+
+class Follower(Base):
+    __tablename__='Follower'
+    id = Column(Integer, primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('User.id'))
+    user_from_id = Column(Integer, ForeignKey('User.id'))
 
     def to_dict(self):
         return {}
@@ -35,3 +89,4 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+
